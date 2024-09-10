3164505871c8ee7e82dbd9eba8a3978158ab7b87
Kashyap <kashyap.mistry.btech2021@sitpune.edu.in>
2024-09-10 19:47:16 +0530

    commit7

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import SearchBar from './components/SearchBar';
 import Navbar from './components/Navbar';
 import Logo from './components/RTLogo';
 import RidePage from './components/RidePage';
-import ImageSlider from './components/ImageSlider'; // Import the ImageSlider component
+import ImageSlider from './components/ImageSlider';
 import './App.css';
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home');
+  const [isScrollLocked, setIsScrollLocked] = useState(true);
+
+  React.useEffect(() => {
+    if (isScrollLocked) {
+      document.body.classList.add('lock-scroll');
+    } else {
+      document.body.classList.remove('lock-scroll');
+    }
+  }, [isScrollLocked]);
 
   return (
     <div className="app">
+      <div className="background"></div> {/* Add the background div */}
       {currentPage === 'home' && (
-        <>
-          <header className="header">
-            <Logo />
-            <SearchBar setCurrentPage={setCurrentPage} />
-          </header>
-          <ImageSlider scale={0.8} position="center" /> {/* Example scaling and positioning */}
-        </>
+        <header className="header">
+          <Logo />
+          <SearchBar setCurrentPage={setCurrentPage} />
+        </header>
       )}
+      {currentPage === 'home' && <ImageSlider />}
       {currentPage === 'ride' && <RidePage />}
       <Navbar setCurrentPage={setCurrentPage} />
     </div>
